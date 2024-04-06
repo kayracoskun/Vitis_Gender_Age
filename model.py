65601e48a29e746f2ecee7431e04910d5f31c7ce
kayra.coskun <kayra.coskun@outlook.com.tr>
2024-04-06 13:15:25 -0600

    added gender model

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, BatchNormalization, Flatten, Dense, Dropout
+from tensorflow.keras.layers import Input, SeparableConv2D, Conv2D, MaxPooling2D, BatchNormalization, Flatten, Dense, Dropout
 from tensorflow.keras.models import Model
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
@@ -12,6 +12,8 @@ from sklearn.preprocessing import LabelEncoder
 from tensorflow_model_optimization.quantization.keras import vitis_quantize
 from tensorflow.keras.utils import plot_model
 
+
+"""
 def custom_data_generator(directory, target_size, batch_size):
     gender_labels = {'female': 0, 'male': 1}
     age_labels = ['age_0-9', 'age_10-19', 'age_20-29', 'age_30-39', 'age_40-49', 'age_50-59', 'age_60-69', 'age_70-79', 'age_80-89', 'age_90-99', 'age_100-116']
@@ -36,12 +38,39 @@ def custom_data_generator(directory, target_size, batch_size):
                 if len(image_batch) == batch_size:
                     yield (np.array(image_batch), {'gender_output': to_categorical(gender_label_batch, num_classes=len(gender_labels)), 'age_output': to_categorical(age_label_batch, num_classes=len(age_labels))})
                     image_batch, gender_label_batch, age_label_batch = [], [], []
+"""
+
+
+def custom_data_generator(directory, target_size, batch_size):
+    gender_labels = {'female': 0, 'male': 1}
+    
+    while True:
+        image_batch = []
+        gender_label_batch = []
+        for gender in gender_labels.keys():
+            gender_folder_path = os.path.join(directory, gender)
+            # Iterate through each sub-folder in the gender directory
+            for age_group in os.listdir(gender_folder_path):
+                age_group_folder_path = os.path.join(gender_folder_path, age_group)
+                # Check if it's indeed a directory
+                if not os.path.isdir(age_group_folder_path):
+                    continue
+                for img_file in os.listdir(age_group_folder_path):
+                    img_path = os.path.join(age_group_folder_path, img_file)
+                    img = load_img(img_path, target_size=target_size)
+                    img = img_to_array(img) / 255.0  # Normalize image
+
+                    image_batch.append(img)
+                    gender_label_batch.append(gender_labels[gender])
+
+                    if len(image_batch) == batch_size:
+                        # Only yield gender labels for classification
+                        yield (np.array(image_batch), to_categorical(gender_label_batch, num_classes=len(gender_labels)))
+                        image_batch, gender_label_batch = [], []  # Reset for the next batch
+
 
 # Define the input size for the model
 input_shape = (224, 224, 3)
-
-# Define the number of age classes
-num_age_classes = 11
 
 # Define the number of gender classes
 num_gender_classes = 2
@@ -50,17 +79,17 @@ num_gender_classes = 2
 input_layer = Input(shape=input_shape)
 
 # First convolutional block
-x = Conv2D(32, (3,3), activation='relu', padding='same')(input_layer)
+x = Conv2D(16, (3,3), activation='relu', padding='same')(input_layer)
 x = MaxPooling2D((2,2))(x)
 x = BatchNormalization()(x)
 
 # Second convolutional block
-x = Conv2D(64, (3,3), activation='relu', padding='same')(x)
+x = Conv2D(32, (3,3), activation='relu', padding='same')(x)
 x = MaxPooling2D((2,2))(x)
 x = BatchNormalization()(x)
 
 # Third convolutional block
-x = Conv2D(128, (3,3), activation='relu', padding='same')(x)
+x = Conv2D(64, (3,3), activation='relu', padding='same')(x)
 x = MaxPooling2D((2,2))(x)
 x = BatchNormalization()(x)
 
@@ -68,25 +97,24 @@ x = BatchNormalization()(x)
 x = Flatten()(x)
 
 # Gender classification branch
-gender_branch = Dense(64, activation='relu')(x)
-gender_branch = Dropout(0.5)(gender_branch)
-gender_branch = Dense(num_gender_classes, activation='softmax', name='gender_output')(gender_branch)
+x = Dense(32, activation='relu')(x)
+x = Dropout(0.5)(x)
+gender_output = Dense(num_gender_classes, activation='softmax', name='gender_output')(x)
 
-# Age regression branch
-age_branch = Dense(64, activation='relu')(x)
-age_branch = Dropout(0.5)(age_branch)
-age_branch = Dense(32, activation='relu')(age_branch)
-age_branch = Dropout(0.5)(age_branch)
-age_branch = Dense(num_age_classes, activation='softmax', name='age_output')(age_branch)
+# Define the model
+model = Model(inputs=input_layer, outputs=gender_output)
 
-# Define the multi-output model
-model = Model(inputs=input_layer, outputs=[gender_branch, age_branch])
-
+"""
 # Compile the model
 model.compile(optimizer='adam',
               loss={'gender_output': 'categorical_crossentropy', 'age_output': 'categorical_crossentropy'},
               metrics={'gender_output': 'accuracy', 'age_output': 'mae'})
+"""
 
+# Compile the model
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
 
 train_generator = custom_data_generator(
     directory='./data/UTKFace',
@@ -94,9 +122,10 @@ train_generator = custom_data_generator(
     batch_size=32
 )
 
-model.fit(train_generator, epochs=10, steps_per_epoch=200)  # Adjust steps_per_epoch based on your dataset size
-
 model.summary()
+
+model.fit(train_generator, epochs=2, steps_per_epoch=200)  # Adjust steps_per_epoch based on your dataset size
+
 plot_model(model, to_file='gender_age_model.png', show_shapes=True, show_layer_names=True)
 
 N = 500  # Number of images you want to extract
@@ -124,4 +153,4 @@ quantizer = vitis_quantize.VitisQuantizer(model)
 quantized_model = quantizer.quantize_model(calib_dataset=X_train[0:500])
 
 # Save the quantized model
-quantized_model.save("quantized_gender_age_model.h5")
+quantized_model.save("quantized_gender_model.h5")
